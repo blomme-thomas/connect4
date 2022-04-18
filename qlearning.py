5c866f39c19a746fd3364929e4f80913e6eb7c5d
Thomas <blommethomas59@gmail.com>
2022-04-18 21:49:53 +0200

    update chemin

--- qlearning.py
+++ qlearning.py
@@ -63,5 +63,39 @@ def update_case(r, f, grid, i, j):
     grid[i,j] = max
     return
 
+def update_chemin(r, f, grid, l):
+    temp = grid.copy
+    n, m = grid.shape
+    for (i,j) in l:
+        if i>0:
+            if max<(r+f*grid[i-1,j]):
+                max = r+f*grid[i-1,j]
+            if j>0:
+                if max<(r+f*grid[i-1,j-1]):
+                    max = r+f*grid[i-1,j-1]
+            if j<n-1:
+                if max<(r+f*grid[i-1,j+1]):
+                    max = r+f*grid[i-1,j+1]
+        if i<n-1:
+            if max<(r+f*grid[i+1,j]):
+                max = r+f*grid[i+1,j]
+            if j>0:
+                if max<(r+f*grid[i+1,j-1]):
+                    max = r+f*grid[i+1,j-1]
+            if j<n-1:
+                if max<(r+f*grid[i-1,j+1]):
+                    max = r+f*grid[i-1,j+1]
+        if j>0:
+            if max<(r+f*grid[i,j-1]):
+                max = r+f*grid[i,j-1]
+        if j<m-1:
+            if max<(r+f*grid[i,j+1]):
+                max = r+f*grid[i,j+1]
+        temp[i,j] = max
+        f -= f/(len(l)-1)
+    for (i,j) in l:
+        grid[i,j] = temp[i,j]
+    return
+
 
 
